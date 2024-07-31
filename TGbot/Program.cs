29840c0b665bd60b1b62782314a0a5682fd0482b
Name <email@example.ru>
2024-08-01 00:19:55 +0300

    TgBot

--- TGbot/Program.cs
+++ TGbot/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using TelegramBot;
+using VoiceTexterBot.Services;
+using TextMessageController;
+using VoiceTexterBot.Controllers;
+
+namespace VoiceTexterBot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddTransient<TextFunction>();
+            services.AddSingleton<IStorage, MemoryStorage>();  
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<DefaultMessageController>;
+            services.AddTransient<InlineKeyboardController>;
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7307550605:AAHPSiu5VLY3J41afiHV9x-OtDLH798VGSw"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
---
+++ TGbot/Unities/TextFunction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using VoiceTexterBot;
+
+namespace TelegramBot
+{
+    class TextFunction
+    {
+
+
+        public class StringAndNumberUtils
+        {
+            // Метод для определения длины строки
+            public int GetStringLength(string text)
+            {
+                if (text == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return text.Length;
+                }
+            }
+
+            // Метод для вычисления суммы чисел
+            public int SumNumbers(string numbersString)
+            {
+                string[] numbers = numbersString.Split(' ');
+
+                int sum = 0;
+                foreach (string number in numbers)
+                {
+                    if (int.TryParse(number, out int parsedNumber))
+                    {
+                        sum += parsedNumber;
+                    }
+                }
+                return sum;
+            }
+        }
+    }
+}
